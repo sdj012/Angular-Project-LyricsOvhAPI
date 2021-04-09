05856bd6e609021dd9c22ac34dcd3592c2723cf0
sdj012 <sdjeong012@gmail.com>
2021-04-09 06:01:06 -0400

    add forms to entry

--- src/app/entry/entry.component.ts
+++ src/app/entry/entry.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-entry',
   templateUrl: './entry.component.html',
   styleUrls: ['./entry.component.css']
 })
+
 export class EntryComponent implements OnInit {
 
-  constructor() { }
+  // public lyricForm: FormGroup;
 
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+  ) 
+    {}
+
+
+  // artistControl= new FormControl('');
+  // songControl= new FormControl('');
+
+  // submitted = false;
+
+  onSubmit() { 
+    // this.submitted = true; 
+    console.log("submitted: ", )
+    // this.displayData();
+
   }
 
+  displayData(){
+    // console.log("this.lyricForm: ", this.lyricForm.value);
+    // this.lyricForm.reset();
+  }
+
+
+
+  ngOnInit() {}
+
+  lyricForm = this.formBuilder.group({
+    'artist': new FormControl(''),
+    'song': new FormControl('')
+  });
+
+
 }
+
+
+
