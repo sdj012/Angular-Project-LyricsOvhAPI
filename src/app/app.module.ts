726400d3d7daa0b4b46ab8f2c3da723fd18e425f
sdj012 <sdjeong012@gmail.com>
2021-04-09 22:48:00 -0400

    create searchform component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,18 @@ import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ContentsComponent } from './contents/contents.component';
 import { LyricsComponent } from './lyrics/lyrics.component';
-import { EntryComponent } from './entry/entry.component'; //Mechanism for communicating ewith a remote server over HTTP
+import { EntryComponent } from './entry/entry.component';
+import { SearchformComponent } from './searchform/searchform.component'; //Mechanism for communicating ewith a remote server over HTTP
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ContentsComponent,
     LyricsComponent,
-    EntryComponent
+    EntryComponent,
+    SearchformComponent
   ],
   imports: [
     BrowserModule,
